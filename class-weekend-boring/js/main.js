1ef981e6670352dbc15106e559d2b86f5989aad6
Liam Mandeville <liamman1990@gmail.com>
2022-04-27 18:07:02 -0700

    Finished homework, place the response into the DOM

--- class-weekend-boring/js/main.js
+++ class-weekend-boring/js/main.js
@@ -1,16 +1,14 @@
 document.querySelector('#check').addEventListener('click', check)
 
 function check() {
-
-  let day = document.querySelector('#day').value.ToLowerCase
-  day = day.ToLowerCase
-  //Conditionals go here
-if(day === 'Tuesday' || day === "Thursday"){
-  console.log('CLASS DAY !!!') // put into the DOM for HW
-}else if(day === "Saturday" || day === "Sunday") {
-  console.log('Weekend')
-}else {
-  console.log('BOOORINGGGGG')
+  const day = document.querySelector('#day').value.toLowerCase()
+  
+  if(day === 'tuesday' || day === "thursday"){
+    document.querySelector('#placeToSee').innerHTML = 'CLASS TODAY !!!';
+  }else if(day === "saturday" || day === "sunday") {
+    document.querySelector('#placeToSee').innerHTML = 'Weekend !'
+  }else {
+    document.querySelector('#placeToSee').innerHTML = 'BBBOOORRRIINNNGGGG'
+  }
 }
-
-}
+// place response into the DOM for homework
