e70de122635701b676c4cd4108e888f530d31680
Liam Mandeville <liamman1990@gmail.com>
2022-04-30 18:40:20 -0700

    added calc pic, more buttons for functionality

--- simple-calculator/js/main.js
+++ simple-calculator/js/main.js
@@ -2,25 +2,93 @@ let total = 0
 
 document.querySelector('#makeZero').addEventListener('click', clearCalc)
 document.querySelector('#num1').addEventListener('click', input1)
-document.querySelector('#addNine').addEventListener('click', add9)
-document.querySelector('#minusTwo').addEventListener('click', sub2)
+document.querySelector('#num2').addEventListener('click', input2)
+document.querySelector('#num3').addEventListener('click', input3)
+document.querySelector('#num4').addEventListener('click', input4)
+document.querySelector('#num5').addEventListener('click', input5)
+document.querySelector('#num6').addEventListener('click', input6)
+document.querySelector('#num7').addEventListener('click', input7)
+document.querySelector('#num8').addEventListener('click', input8)
+document.querySelector('#num9').addEventListener('click', input9)
+document.querySelector('#plus').addEventListener('click', addition)
+document.querySelector('#minus').addEventListener('click', subtraction)
+document.querySelector('#plus').addEventListener('click', addition)
+document.querySelector('#plus').addEventListener('click', addition)
 
 function clearCalc() {
   total = 0
   document.querySelector('#Result').innerText = total
 }
-
+function input1() {
+  total = total + 1
+  document.querySelector('#Result').innerText = total
+}
+function input2() {
+  total = total + 2
+  document.querySelector('#Result').innerText = total
+}
+function input3() {
+  total = total + 3
+  document.querySelector('#Result').innerText = total
+}
+function input4() {
+  total = total + 4
+  document.querySelector('#Result').innerText = total
+}
+function input5() {
+  total = total + 5
+  document.querySelector('#Result').innerText = total
+}
+function input6() {
+  total = total + 6
+  document.querySelector('#Result').innerText = total
+}
+function input7() {
+  total = total + 7
+  document.querySelector('#Result').innerText = total
+}
+function input8() {
+  total = total + 8
+  document.querySelector('#Result').innerText = total
+}
+function input9() {
+  total = total + 9
+  document.querySelector('#Result').innerText = total
+}
+function addition() {
+  total = + total 
+  document.querySelector('#Result').innerText = total
+}
+function subtraction() {
+total = - total
+document.querySelector('#Result').innerText = total
+}
+function input1() {
+  total = total + 1
+  document.querySelector('#Result').innerText = total
+}
+function input1() {
+  total = total + 1
+  document.querySelector('#Result').innerText = total
+}
+function input1() {
+  total = total + 1
+  document.querySelector('#Result').innerText = total
+}
+function input1() {
+  total = total + 1
+  document.querySelector('#Result').innerText = total
+}
+function input1() {
+  total = total + 1
+  document.querySelector('#Result').innerText = total
+}
+function input1() {
+  total = total + 1
+  document.querySelector('#Result').innerText = total
+}
 function input1() {
   total = total + 1
   document.querySelector('#Result').innerText = total
 }
 
-function add9() {
-  total = total + 9
-  document.querySelector('#Result').innerHTML = total
-}
-
-function sub2() {
-  total = total - 2
-  document.querySelector('#Result').innerHTML = total
-}
